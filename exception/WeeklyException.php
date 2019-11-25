a54cf4d7106e2f18d9ebe4194f5eecea40572d0f
baizhaoyun <1027588493@qq.com>
2019-11-25 11:30:08 +0800

    删除只是

--- exception/WeeklyException.php
+++ exception/WeeklyException.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User : baizhaoyun
- * Email: baizhaoyun@rong360.com
- * Date : 2019/11/19
- * Time : 17:00
- */
 namespace Awesome\Exception;
 
 class WeeklyException extends \Exception
